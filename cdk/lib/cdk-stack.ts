31f90e2a805ad1daeb88b5ee18e1d84691d59d37
Masatomi KINO <masatomix@ki-no.org>
2023-07-22 09:58:38 +0900

    VPCを追加するサンプルと、外部ファイルから環境設定を受け取るサンプル

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CfnVPC } from 'aws-cdk-lib/aws-ec2'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
@@ -12,5 +13,29 @@ export class CdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const argKey = 'profile'
+    const tmpStr = this.node.tryGetContext(argKey)
+    const profileStr = tmpStr ? tmpStr : 'dev'
+    const profile = this.node.tryGetContext(profileStr)
+
+    // console.log('--')
+    // console.log(profileStr)
+    // console.log(profile)
+    // console.log(profile.name)
+    // console.log(profile['name'])
+    // console.log('--')
+
+    // VPC
+    const vpc = new CfnVPC(this, `${profile.name}-VPC`, {
+      // cfnのリソース名
+      cidrBlock: '192.168.0.0/16',
+      tags: [
+        {
+          key: 'Name',
+          value: `${profile.name}-vpc`,
+        },
+      ],
+    })
   }
 }
