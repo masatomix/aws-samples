60f58766be02b5811b7d86a535552d13e5dd3c80
Masatomi KINO <masatomix@ki-no.org>
2023-07-24 01:54:06 +0900

    VPCを構築するサンプル その上にSecurityGroupとEC2インスタンスを作るサンプルを追加。

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { VPCStack, EC2Stack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+const vpcStack = new VPCStack(app, 'VPCStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -19,3 +19,6 @@ new CdkStack(app, 'CdkStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+
+new EC2Stack(app, 'EC2Stack', vpcStack.vpc, vpcStack.publicSubnets[0]);
